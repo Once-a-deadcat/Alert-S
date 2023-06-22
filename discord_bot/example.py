bbbf75c89a87a3cdf8346b5ecceb0e3605b9e45d
Once-a-deadcat <catgorogoro994@gmail.com>
2023-06-22 20:37:01 +0900

    [add] formater

--- discord_bot/example.py
+++ discord_bot/example.py
@@ -10,8 +10,12 @@ CONNECTION_STRING = os.environ["CONNECTION_STRING"]
 CONTAINER_NAME = os.environ["CONTAINER_NAME"]
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-handler = AzureBlobHandler(connection_string=CONNECTION_STRING,container_name=CONTAINER_NAME, blob_name_prefix='log')
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+handler = AzureBlobHandler(
+    connection_string=CONNECTION_STRING,
+    container_name=CONTAINER_NAME,
+    blob_name_prefix="log",
+)
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
@@ -22,19 +26,22 @@ intents.message_content = True
 # クライアントの生成
 client = discord.Client(intents=intents)
 
+
 def format_device_list(device_data):
-    device_list = device_data['body']['deviceList']
+    device_list = device_data["body"]["deviceList"]
     formatted_list = []
     for device in device_list:
-        cloud_service_status = device.get('enableCloudService', 'N/A')
-        device_info = f"### deviceName: {device['deviceName']} \n\tdeviceId: {device['deviceId']} \n\thubDeviceId: {device['hubDeviceId']} \n\tenableCloudService: {cloud_service_status} \n\t Type: {device['deviceType']}"
+        cloud_service_status = device.get("enableCloudService", "N/A")
+        device_info = f"### deviceName: {device['deviceName']} \n\tdeviceId: {device['deviceId']} \n\thubDeviceId: {device['hubDeviceId']} \n\tenableCloudService: {cloud_service_status} \n\ttype: {device['deviceType']}"
         formatted_list.append(device_info)
-    return '\n'.join(formatted_list)
+    return "\n".join(formatted_list)
+
 
 # discordと接続した時に呼ばれる
 @client.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f"We have logged in as {client.user}")
+
 
 # メッセージを受信した時に呼ばれる
 @client.event
@@ -44,17 +51,18 @@ async def on_message(message):
         return
 
     # メッセージが"$hello"で始まっていたら"Hello!"と応答
-    if message.content.startswith('$hello'):
+    if message.content.startswith("$hello"):
         logger.info("Hello! command received")
-        await message.channel.send('Hello!')
+        await message.channel.send("Hello!")
 
-    if message.content.startswith('$list'):
+    if message.content.startswith("$list"):
         device_list = await get_device_list()
         formatted_list = format_device_list(device_list)
         logger.info(f"list command received")
         logger.info(f"device list: {formatted_list}")
         await message.channel.send(formatted_list)
-    
+
+
 # クライアントの実行
-token = os.environ['DISCORD_TOKEN']
+token = os.environ["DISCORD_TOKEN"]
 client.run(token)
