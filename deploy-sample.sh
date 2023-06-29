5351a63f43666b5bf0fc2e8d0ec0376f9ac55239
Once-a-deadcat <catgorogoro994@gmail.com>
2023-06-30 02:37:22 +0900

    [add] deploy bash sample🍵

--- deploy-sample.sh
+++ deploy-sample.sh
@@ -3,30 +3,36 @@
 # get current timestamp
 timestamp=$(date +%Y%m%d%H%M%S)
 
+# .env fileを読み込む
+export $(egrep -v '^#' .env | xargs)
+
 # ACRにログイン
-docker login imagename.azurecr.io -u username -p password
+docker login acrname.azurecr.io -u acruser -p password
 
-# Dockerビルドコマンドを実行
-docker image build -t imagename:$timestamp .
+# Timestamp有りでDockerビルドコマンドを実行
+docker image build -t alert-s-switchbot:$timestamp .
 
-docker tag imagename:$timestamp imagename.azurecr.io/imagename:$timestamp
-docker push imagename.azurecr.io/imagename:$timestamp
+# ACRにプッシュ
+docker tag alert-s-switchbot:$timestamp acrname.azurecr.io/alert-s:$timestamp
+docker push acrname.azurecr.io/alert-s:$timestamp
 
+# bashからazコマンドを実行するためにログイン
 az login
-# Create or update the container instance
+# コンテナ作る🍵
 az container create \
-  --resource-group rg-name \
-  --name acrname \
-  --image imagename.azurecr.io/imagename:$timestamp \
-  --registry-username username \
+  --resource-group rg \
+  --name container \
+  --image acrname.azurecr.io/alert-s:$timestamp \
+  --registry-username acruser \
   --registry-password password \
   --dns-name-label alerts \
+  --cpu 0.1 \
+  --memory 0.1 \
   --restart-policy Always \
   --secure-environment-variables \
-    API_KEY= \
-    CLIENT_SECRET= \
-    DISCORD_TOKEN= \
-    CONNECTION_STRING= \
-    CONTAINER_NAME= \
-  --cpu 0.1 \
-  --memory 0.1
+    API_KEY="" \
+    CLIENT_SECRET="" \
+    DISCORD_TOKEN="" \
+    CONNECTION_STRING="" \
+    CONTAINER_NAME="" \
+    LED_DEVICE_ID="" \
